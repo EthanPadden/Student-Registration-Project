f171ec1b5fa220f346de8404d4ba50c822053dcc
Ethan Padden <ethanpadden@gmail.com>
2020-11-09 18:16:32 +0000

    Add Course class content

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,2 +1,40 @@
+import java.util.Date;
+import java.util.List;
+
 public class Course {
+    private String name;
+    private List<Module> modules;
+    private List< Student> studentsEnrolled;
+    private Date startDate;
+    private Date endDate;
+
+    public Course( String name, Date startDate, Date endDate ) {
+        setName( name );
+        setStartDate( startDate );
+        setEndDate( endDate );
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate( Date startDate ) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate( Date endDate ) {
+        this.endDate = endDate;
+    }
 }
