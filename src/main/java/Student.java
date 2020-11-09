76be618c6dc81a8d797049cf7cdba32d36be3097
Ethan Padden <ethanpadden@gmail.com>
2020-11-09 17:42:44 +0000

    Add empty Course and Module classes. Add Student class with content

---
+++ src/main/java/Student.java
@@ -0,0 +1,59 @@
+import java.util.Date;
+import java.util.List;
+
+public class Student {
+    private String name;
+    private int age;
+    private Date dob;
+    private long id;
+    private String username;
+    private List< Course > course;
+    private List< Module > modules;
+
+    public Student( long id, String name, int age, Date dob ) {
+        setId( id );
+        setName( name );
+        setAge( age );
+        setDob( dob );
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId( long id ) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge( int age ) {
+        this.age = age;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob( Date dob ) {
+        this.dob = dob;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername( String username ) {
+        this.username = username;
+    }
+}
