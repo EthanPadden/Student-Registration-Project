4e30e790795e73e9984eafb0c5867367821a9b40
Ethan Padden <ethanpadden@gmail.com>
2020-11-22 18:44:07 +0000

    Add methods to add/remove courses and modules to students

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,8 +1,6 @@
 import org.joda.time.DateTime;
 
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 public class Student {
     private String name;
@@ -10,7 +8,7 @@ public class Student {
     private DateTime dob;
     private long id;
     private String username;
-    private ArrayList< Course > course;
+    private ArrayList< Course > courses;
     private ArrayList< Module > modules;
 
     public Student( long id, String name, int age, DateTime dob ) {
@@ -60,19 +58,47 @@ public class Student {
         this.username = username;
     }
 
-    public ArrayList< Course > getCourse() {
-        return course;
+    public ArrayList< Course > getCourses() {
+        return courses;
     }
 
-    public void setCourse( ArrayList< Course > course ) {
-        this.course = course;
+    public void addCourse( Course course ) {
+        // Add new course to the course list
+        courses.add( course );
+
+        // Add all modules from that course to the modules list
+        ArrayList< Module > modulesToAdd = course.getModules();
+        for ( Module module : modulesToAdd ) {
+            addModule( module );
+        }
+    }
+
+    public void removeCourse( Course course ) {
+        // If the course is in the course list, remove it
+        if ( courses.contains( course ) ) {
+            courses.remove( course );
+        }
+
+        // If any of the modules in that course are in the modules list, remove them
+        ArrayList< Module > modulesToRemove = course.getModules();
+        for ( Module module : modulesToRemove ) {
+            removeModule( module );
+        }
     }
 
     public ArrayList< Module > getModules() {
         return modules;
     }
 
-    public void setModules( ArrayList< Module > modules ) {
-        this.modules = modules;
+    public void addModule( Module module ) {
+        // Add new module to the modules list
+        modules.add( module );
+    }
+
+    public void removeModule( Module module ) {
+        // If the module is in the modules list, remove it
+        if ( modules.contains( module ) ) {
+            modules.remove( module );
+        }
     }
 }
